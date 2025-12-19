f10a7f5427d310afc51c902177a66058e6d96673
PhucHoang200 <phucphuc2004444@gmail.com>
2025-12-19 19:21:49 +0700

    solve more problems

--- Test_Api_DB_Connect_Stylo_App/Test_Api_DB_Connect_Stylo_App/obj/Debug/net8.0/Test_Api_DB_Connect_Stylo_App.AssemblyInfo.cs
+++ Test_Api_DB_Connect_Stylo_App/Test_Api_DB_Connect_Stylo_App/obj/Debug/net8.0/Test_Api_DB_Connect_Stylo_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_Api_DB_Connect_Stylo_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa239cbceb9dbf1be17c3db65b31f7c56bb043e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067adeecb67e71f08b3ddf276f932aeaf65bb20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_Api_DB_Connect_Stylo_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_Api_DB_Connect_Stylo_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
